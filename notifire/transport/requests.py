c32a474e7ea487893a8875594d8ad23d01a93e62
Martin Mihál <memonil@memonil.com>
2020-12-27 15:54:58 +0100

    Add response check

--- notifire/transport/requests.py
+++ notifire/transport/requests.py
@@ -3,6 +3,7 @@
 :license: BSD, see LICENSE for more details.
 """
 
+from notifire.exceptions import APIError, RateLimited
 from notifire.transport.http import HTTPTransport
 
 try:
@@ -24,5 +25,22 @@ class RequestsHTTPTransport(HTTPTransport):
             # If SSL verification is enabled, use the provided CA bundle to
             # perform the verification
             self.verify_ssl = self.ca_certs
-        requests.post(url, data, headers,
-                      verify=self.verify_ssl, timeout=self.timeout)
+        try:
+            resp = requests.post(url, data=data, headers=headers,
+                                 verify=self.verify_ssl, timeout=self.timeout)
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as exc:
+            msg = exc.response.text
+            code = exc.response.status_code
+            print(exc.response.headers.get('retry-after'))
+            if code == 429:  # pylint: disable=R1720
+                try:
+                    retry_after = exc.response.headers.get('retry-after')
+                    retry_after = int(retry_after)
+                except (ValueError, TypeError):
+                    retry_after = 0
+                raise RateLimited(msg, retry_after) from exc
+            elif msg:
+                raise APIError(msg, code) from exc
+            else:
+                raise
